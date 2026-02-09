9eb89d19c6f6ae1f53803a3f827f4abfb479394f
Ghesker-afk <spacexz12345@gmail.com>
2026-02-09 15:24:25 -0300

    feat: create the Register Page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
 
 export function Home() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/register" element={<RegisterPage />}></Route>
     </Routes>
   )
 }
--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,11 +3,17 @@ import API from "../config/apiClient";
 // This folder will hold all of our API calls to our auth
 // server.
 
-type LoginData = {
+type LoginUserData = {
   email: string;
   password: string;
 };
 
-export async function login(data: LoginData) {
+type RegisterUserData = LoginUserData & { confirmPassword: string };
+
+export async function login(data: LoginUserData) {
   API.post("/auth/login", data);
+}
+
+export async function register(data: RegisterUserData) {
+  API.post("/auth/register", data);
 }
